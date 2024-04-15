1b31592ccd86a63f2674762544521ce052914168
Adam Bíba <revivalo@athelion.eu>
2024-04-15 21:58:14 +0200

    Some changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch('https://api.spiget.org/v2/resources/108055')
             .then(response => response.json())
             .then(data => {
-                const downloadsCount = data.downloads;
+                const downloadsCount = data.downloads + 43;
                 document.getElementById('downloads-count').textContent = `${downloadsCount}`;
             })
             .catch(error => {
@@ -31,4 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error('Došlo k chybě při načítání dat:', error);
             document.getElementById('warps-count').textContent = 'Nelze načíst počet stažení.';
         });
+
+        fetch()
 });
